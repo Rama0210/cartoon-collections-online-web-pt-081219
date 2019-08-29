6d0abec0ebed01220076b745b2ea8e5ca2bf6239
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-29 20:57:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,4 @@
-def roll_call_dwarves(dwarves)# code an argument here
+def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |x,y|
     name = x
     num = y+1
@@ -6,19 +6,19 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end	
 
-def summon_captain_planet(planeteer_calls)# code an argument here
+def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |x|
     x.capitalize << "!"
   end
 end	
 
-def long_planeteer_calls(calls)# code an argument here
+def long_planeteer_calls
   calls.any? do |x|
     x.length > 4
   end
 end
 
-def find_the_cheese(strings)# code an argument here
+def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]	  
   strings.find do |x|
     cheese_types.include?(x)
